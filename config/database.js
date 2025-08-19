d70fe8e66da991a74a4362157034ce23a6e8380a
lucasarauujo <60785525+lucasarauujo@users.noreply.github.com>
2025-08-19 15:43:16 -0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -17,8 +17,6 @@ const connectDB = async () => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0 // Disable mongoose buffering
     });
 
     console.log('🟢 MongoDB conectado com sucesso!');
